7a0656f6bf97eabe68a47636da0272d823ddfbee
Dave tDot <dartysavan@gmail.com>
2025-03-11 19:16:54 +0100

    replocker-monorepo init

---
+++ replocker-api/src/main/java/com/ap2/replocker/ReplockerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ap2.replocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplockerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplockerApiApplication.class, args);
+	}
+
+}
---
+++ replocker-api/src/test/java/com/ap2/replocker/ReplockerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap2.replocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplockerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
