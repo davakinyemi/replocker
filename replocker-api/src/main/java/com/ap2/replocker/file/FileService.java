f21e02aeaa70dc33517d16a035f285b6cbad473f
Dave <dave.akinyemi@ibykus.de>
2025-03-13 12:51:32 +0100

    add file upload out path to application-dev.yml

---
+++ replocker-api/src/main/java/com/ap2/replocker/file/FileService.java
@@ -0,0 +1,71 @@
+package com.ap2.replocker.file;
+
+
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.io.File.separator;
+import static java.lang.System.currentTimeMillis;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileService {
+    @Value("${application.file.uploads.report-output-path}")
+    private String fileUploadPath;
+
+    public String saveFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String reportCollectionId
+    ) {
+        final String fileUploadSubPath = "report_collection" + separator + reportCollectionId;
+        return this.uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = this.fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create the target folder: " + targetFolder);
+                return null;
+            }
+        }
+
+        final String fileExtension = this.getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + separator + currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File uploaded to: " + targetFilePath);
+            return targetFilePath;
+        } catch (IOException exception) {
+            log.error("File was not uploaded", exception);
+        }
+        return null;
+    }
+
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) return "";
+
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) return "";
+
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
+    }
+}
