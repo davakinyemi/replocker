108990af7a9ca783607eb02592719b8764a77242
Dave tDot <dartysavan@gmail.com>
2025-03-13 20:00:48 +0100

    add openapi security scheme

--- replocker-api/src/main/java/com/ap2/replocker/ReplockerApiApplication.java
+++ replocker-api/src/main/java/com/ap2/replocker/ReplockerApiApplication.java
@@ -1,5 +1,10 @@
 package com.ap2.replocker;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +13,19 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
+@SecurityScheme(
+		name = "keycloak",
+		type = SecuritySchemeType.OAUTH2,
+		bearerFormat = "JWT",
+		scheme = "bearer",
+		in = SecuritySchemeIn.HEADER,
+		flows = @OAuthFlows(
+				password = @OAuthFlow(
+						authorizationUrl = "http://localhost:9090/realms/replocker/protocol/openid-connect/auth",
+						tokenUrl = "http://localhost:9090/realms/replocker/protocol/openid-connect/token"
+				)
+		)
+)
 public class ReplockerApiApplication {
 
 	public static void main(String[] args) {
