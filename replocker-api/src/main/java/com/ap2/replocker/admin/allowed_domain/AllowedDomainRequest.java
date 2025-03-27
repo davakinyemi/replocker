3247955b0c23e13dbc73336de097e7fd82b162e2
Dave <dave.akinyemi@ibykus.de>
2025-03-27 08:47:53 +0100

    remove AT character from domain name validation

--- replocker-api/src/main/java/com/ap2/replocker/admin/allowed_domain/AllowedDomainRequest.java
+++ replocker-api/src/main/java/com/ap2/replocker/admin/allowed_domain/AllowedDomainRequest.java
@@ -6,12 +6,13 @@ import jakarta.validation.constraints.Pattern;
 public record AllowedDomainRequest(
         @NotBlank(message = "Domain cannot be blank")
         @Pattern(
-                regexp = AllowedDomainRequest.ALLOWED_DOMAIN_PATTERN, // "^@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$\n",
-                message = "Invalid domain format (e.g., @domain.xyz, @24x7care.com, @domain2024.xyz)",
+                regexp = AllowedDomainRequest.ALLOWED_DOMAIN_PATTERN,
+                message = "Invalid domain format (e.g., domain.xyz, 24x7care.com, domain2024.xyz)",
                 flags = Pattern.Flag.CASE_INSENSITIVE
         )
         String domainName
 ) {
         private static final String ALLOWED_DOMAIN_PATTERN =
-                "^@[a-zA-Z0-9_](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$";
+                "^[a-zA-Z0-9_](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$";
+                // "^@[a-zA-Z0-9_](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$";
 }
